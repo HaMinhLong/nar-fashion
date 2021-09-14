fe433117ac23509d292a9a8be487d321e7acfb1f
HaMinhLong <haminhlong3@gmail.com>
2021-09-14 07:23:35 +0700

    no message

--- src/pages/Dashboard/Home.js
+++ src/pages/Dashboard/Home.js
@@ -7,9 +7,20 @@ const List = () => {
   return (
     <HeaderContent title="Dashboard">
       <Card
+        bodyStyle={{
+          textAlign: "left",
+          height: "100px",
+          padding: "0 24px",
+        }}
+        style={{
+          boxShadow: "rgba(0, 0, 0, 0.16) 0px 0px 5px",
+          background: "#FF5370",
+          color: "white",
+        }}
         title="Thông tin hồ sơ ứng viên"
         extra={<Link to="/cv">Chi tiết</Link>}
         style={{ width: 500 }}
+        bordered={false}
       >
         <p>Tên ứng viên: Hà Minh Long</p>
         <p>Email: haminhlong3@gmail.com</p>
